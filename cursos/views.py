356060ae8a8dc32fbdd41bfb0160a66253eebb0a
Felipe Benigno <felipe.benigno@greenant.com.br>
2022-05-12 16:33:54 -0300

    Fix urls

--- cursos/views.py
+++ cursos/views.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.decorators import login_required
+
+from django.http import HttpResponse
+from django.template import loader
 
 
+@login_required(login_url="/login/")
+def index(request):
+    context = {'segment': 'index'}
 
+    html_template = loader.get_template('cursos/index.html')
+    return HttpResponse(html_template.render(context, request))
